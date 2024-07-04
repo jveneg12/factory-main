3d94119a81ffac2daec36f8f8e65d2b072536877
jveneg12 <jasoncristian.venegas@gmail.com>
2024-07-04 01:06:36 -0400

    actualizacion2 --conexion y guardado de base de datos

--- factory/views.py
+++ factory/views.py
@@ -1,5 +1,9 @@
 # factory/views.py
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
+from django.utils import timezone
+# from django.contrib.auth.decorators import login_required
 
 def index(request):
     context = {"clase": "index"}
@@ -30,8 +34,30 @@ def home_office(request):
 def carrito(request):
     return render(request, 'demo/carrito.html')
 
+
+
 def registrarse(request):
-    return render(request, 'demo/registrarse.html')
+    if request.method != "POST":
+        context={"clase": "registrarse"}
+        return render(request, 'demo/registrarse.html', context)
+    else:
+        nombre = request.POST["nombre"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = User.objects.create_user(nombre, email, password)
+        user.save()
+        context={"clase": "registro", "mensaje":"Los datos fueron registrados"}
+        return render(request, 'demo/registrarse.html', context)
+
+def get_current_users():
+    active_sessions = Session.objects.filter(expire_date__gte=timezone.now())
+    user_id_list = []
+    for session in active_sessions:
+        data = session.get_decoded()
+        user_id_list.append(data.get('_auth_user_id', None))
+    # Query all logged in users based on id list
+    return User.objects.filter(id__in=user_id_list)
+
 
 def transbank(request):
     return render(request, 'demo/transbank.html')
